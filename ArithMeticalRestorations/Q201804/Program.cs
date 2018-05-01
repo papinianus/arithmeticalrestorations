82e537d70304f082a86f6b1feb9430a104f2a98a
papinianus <dglmsksooah@gmail.com>
2018-05-01 12:28:17 +0900

    106 q1

---
+++ ArithMeticalRestorations/Q201804/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q201804
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">");
+            Q20180429_106.SolveQ1();
+            Console.WriteLine(">");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ArithMeticalRestorations/Q201804/Q20180429_106.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods;
+
+namespace Q201804
+{
+    static class Q20180429_106
+    {
+        internal static void SolveQ1()
+        {
+            foreach(var i in Enumerator.FromOneToNine())
+            {
+                foreach (var j in Enumerator.FromOneToNine())
+                {
+                    var sum = (new[] { i, 4 }).ToDecade() * j;
+                    if (sum / 10 == 29) { Console.WriteLine($"{i}4 {j} {sum}"); }
+                }
+            }
+        }
+    }
+}
