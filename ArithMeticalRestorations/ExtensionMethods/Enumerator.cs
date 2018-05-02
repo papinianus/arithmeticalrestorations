bdab511acfec8310b9c2fdc6beb373f9ad290c91
papinianus <dglmsksooah@gmail.com>
2018-05-02 17:25:25 +0900

    fix enumeration

--- ArithMeticalRestorations/ExtensionMethods/Enumerator.cs
+++ ArithMeticalRestorations/ExtensionMethods/Enumerator.cs
@@ -8,7 +8,7 @@ namespace ExtensionMethods
     {
         #region enumeration of single digit
         public static IEnumerable<int> FromOneToNine() => Range(1, 9);
-        public static IEnumerable<int> FromZeroToNine() => Range(0, 9);
+        public static IEnumerable<int> FromZeroToNine() => Range(0, 10);
         #endregion enumerator of single digit
         #region enumeration of specified digits
         public static IEnumerable<int> NumberInNdigits(int n) => Range((int)Pow(10, n - 1), (int)Pow(10, n) - (int)Pow(10, n - 1));
