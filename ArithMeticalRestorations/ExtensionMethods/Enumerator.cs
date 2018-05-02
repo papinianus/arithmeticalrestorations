2f87b57331db7b3962447937bcbe280d06c7fd89
papinianus <dglmsksooah@gmail.com>
2018-05-02 14:57:05 +0900

    upsert extension methods

--- ArithMeticalRestorations/ExtensionMethods/Enumerator.cs
+++ ArithMeticalRestorations/ExtensionMethods/Enumerator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using static System.Linq.Enumerable;
+using static System.Math;
 
 namespace ExtensionMethods
 {
@@ -8,6 +9,9 @@ namespace ExtensionMethods
         #region enumeration of single digit
         public static IEnumerable<int> FromOneToNine() => Range(1, 9);
         public static IEnumerable<int> FromZeroToNine() => Range(0, 9);
-        #endregion enumerator
+        #endregion enumerator of single digit
+        #region enumeration of specified digits
+        public static IEnumerable<int> NumberInNdigits(int n) => Range((int)Pow(10, n - 1), (int)Pow(10, n) - (int)Pow(10, n - 1));
+        #endregion enumeration of specified digits
     }
 }
