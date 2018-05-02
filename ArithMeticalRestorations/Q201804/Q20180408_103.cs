3a5ad7e979d7a48cfaec5a437aa1110c2254716d
papinianus <dglmsksooah@gmail.com>
2018-05-02 22:29:58 +0900

    103 finish

--- ArithMeticalRestorations/Q201804/Q20180408_103.cs
+++ ArithMeticalRestorations/Q201804/Q20180408_103.cs
@@ -69,52 +69,47 @@ namespace Q201804
         }
         internal static void SolveQ4()
         {
-            foreach (var i in Enumerator.FromOneToNine())
+            foreach (var nums in Enumerator.Permutations(Enumerator.FromZeroToNine(), 6))
             {
-                foreach (var j in Enumerator.FromOneToNine())
-                {
-                    if (i == j) { continue; }
-                    foreach (var k in Enumerator.FromOneToNine())
-                    {
-                        if (i == k || j == k) { continue; }
-                        var first = (new[] { i, j, k }).ToDecade();
-                        if (!((int)(first * k)).Is4digits()) { continue; }
-                        if (!((int)(first * j)).Is3digits()) { continue; }
-                        var second = (new[] { j, k, j, k }).ToDecade();
-                        var ans = (int)(first * second);
-                        if (!ans.Is7digits()) { continue; }
-                        var mochi = (int)(new[] { j, k }).ToDecade();
-                        if (ans % 100 != mochi) { continue; }
-//                        Console.WriteLine($"{first} {second} {first * second}");
-                    }
-                }
+                var bi = nums.ElementAt(0);
+                var bu = nums.ElementAt(1);
+                var n  = nums.ElementAt(2);
+                var se = nums.ElementAt(3);
+                var ki = nums.ElementAt(4);
+                var i  = nums.ElementAt(5);
+                var first = (int)(new[] { bi, bu, n }).ToDecade();
+                if (!(first * se).Is4digits()) { continue; }
+                if (!(first * ki).Is4digits()) { continue; }
+                if (!(first * bu).Is4digits()) { continue; }
+                if (!(first * n).Is4digits()) { continue; }
+                var second = (int)(new[] { se, ki, bu, n }).ToDecade();
+                var feelsgood = (int)(new[] { i, i, ki, bu, n }).ToDecade();
+                var ans = first * second;
+                if((ans % 100000) != feelsgood) { continue; }
+                Console.WriteLine($"{first} {second} {ans}");
             }
         }
         internal static void SolveQ5()
         {
-            foreach (var first in Enumerator.NumberInNdigits(5))
+            foreach (var first in Enumerator.NumberInNdigits(7))
             {
-                foreach (var m in Enumerator.FromOneToNine())
+                foreach (var j in Enumerator.FromOneToNine())
                 {
-                    if(!(first * m).Is123XXX()) { continue; }
-                    foreach(var k in Enumerator.FromOneToNine())
+                    if(!(first * j).IsXX567XX()) { continue; }
+                    foreach(var i in Enumerator.FromOneToNine())
                     {
-                        if (!(first * k).IsXX567X()) { continue; }
-                        foreach (var i in Enumerator.FromOneToNine())
+                        var line1 = first * i;
+                        if (!line1.IsXXXX1X2()) { continue; }
+                        foreach (var k in Enumerator.FromOneToNine())
                         {
-                            if (!(first * i).IsXXXX9X()) { continue; }
-                            foreach (var j in Enumerator.FromOneToNine())
-                            {
-                                if (!(first * j).IsXXX8XX()) { continue; }
-                                foreach (var l in Enumerator.FromOneToNine())
-                                {
-                                    if (!(first * l).IsX4XXXX()) { continue; }
-                                    var second = (new int[] { i, j, k, l, m }).ToDecade();
-                                    var ans = first * second;
-                                    if(!ans.IsXXXX0XXXXX()) { continue; }
-                                    Console.WriteLine($"{first} {second} {first * second}");
-                                }
-                            }
+                            if (!(first * k).Is8XXX9XX()) { continue; }
+                            var second = (new int[] { i, 0, j, k }).ToDecade();
+                            var ans = first * second;
+                            if (!ans.IsXXXXX0XXXX()) { continue; }
+                            var headOfAns = ans / 1000;
+                            var diff = headOfAns - line1;
+                            if(!((int)diff).IsXX3X4()) { continue; }
+                            Console.WriteLine($"{first} {second} {first * second}");
                         }
                     }
                 }
