cde943b87c413ef25418e0653588cdef1f4b7d66
papinianus <dglmsksooah@gmail.com>
2018-05-02 21:53:10 +0900

    Permutation from stackoverflow

--- ArithMeticalRestorations/ExtensionMethods/Enumerator.cs
+++ ArithMeticalRestorations/ExtensionMethods/Enumerator.cs
@@ -13,5 +13,11 @@ namespace ExtensionMethods
         #region enumeration of specified digits
         public static IEnumerable<int> NumberInNdigits(int n) => Range((int)Pow(10, n - 1), (int)Pow(10, n) - (int)Pow(10, n - 1));
         #endregion enumeration of specified digits
+        public static IEnumerable<IEnumerable<T>> Permutations<T>(IEnumerable<T> list, int length)
+        {//https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return Permutations(list, length - 1).SelectMany(t => list.Where(e => !t.Contains(e)), (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
     }
 }
